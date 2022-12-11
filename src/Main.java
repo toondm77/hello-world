45eb135408bfea5af08988f68da8585f5243c17e
toondm77 <r0885209@student.thomasmore.be>
2022-12-11 23:06:19 +0100

    er is een methode toegevoeg met extra code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+    public void eenmethode(int getal){
+        if(getal%2==0){
+            System.out.println("het getal is even");
+        }
+        else{
+            System.out.println("het getal is oneven");
+        }
+    }
     public static void main(String[] args) {
+        Main m = new Main();
+        m.eenmethode(29);
+        m.eenmethode(22);
         System.out.println("Hello world!");
+
     }
 }
